863364b348602805bf3c6beceb38185de158bb93
hwanggyuun <gka365@naver.com>
2025-07-20 20:58:18 +0900

    [init]svg 추가

---
+++ react_native/src/assets/emojis/index.ts
@@ -0,0 +1,7 @@
+export { default as NoEntry } from '@/assets/emojis/noentry.svg';
+export { default as RedExclamation } from '@/assets/emojis/redexclamation.svg';
+export { default as Bell } from '@/assets/emojis/bell.svg';
+export { default as Checkmark } from '@/assets/emojis/checkmark.svg';
+export { default as Dove } from '@/assets/emojis/dove.svg';
+export { default as Envelope } from '@/assets/emojis/envelope.svg';
+export { default as wavinghand } from '@/assets/emojis/wavinghand.svg';
