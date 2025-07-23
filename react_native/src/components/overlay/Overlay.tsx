215b8fa9bd7571177d5f4ce06a64cc2dd1d76726
hwanggyuun <gka365@naver.com>
2025-07-23 16:17:59 +0900

    style: use semantic color and number

--- react_native/src/components/overlay/Overlay.tsx
+++ react_native/src/components/overlay/Overlay.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, StyleSheet, Modal } from 'react-native';
+import { semanticColor } from '@/styles/semantic-color';
 
 interface OverlayProps {
     visible: boolean;
@@ -43,8 +44,7 @@ const Overlay = ({
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'var(--surface-alpha-black-light, rgba(0, 0, 0, 0.25))',
-
+        backgroundColor: semanticColor.overlay.black25,
         justifyContent: 'center',
         alignItems: 'center',
     },
