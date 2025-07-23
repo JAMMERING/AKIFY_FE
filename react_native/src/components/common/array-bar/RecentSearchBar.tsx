06970f4b068b760999789d20f84afd824a7a33b7
sooooa16 <156293624+sooooa16@users.noreply.github.com>
2025-07-23 23:31:45 +0900

    ✨ Feat: RecentSearchBar 컴포넌트 구현

---
+++ react_native/src/components/common/array-bar/RecentSearchBar.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from "react-native"
+import VariantButton from "../button/VariantButton"
+import { semanticNumber } from "@/styles/semantic-number"
+import { semanticColor } from "@/styles/semantic-color"
+import { fonts } from "@/styles/fonts"
+
+const RecentSearchBar = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>최근 검색어</Text>
+      <VariantButton theme="sub" onPress={() => console.log()}>전체 삭제</VariantButton>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 11,
+        paddingHorizontal: semanticNumber.spacing[16]
+    },
+    text: {
+        color: semanticColor.text.tertiary,
+        fontFamily: fonts.family.medium,
+        fontSize: fonts.size.SM,
+        lineHeight: fonts.lineHeight.MD
+    }
+})
+
+export default RecentSearchBar
